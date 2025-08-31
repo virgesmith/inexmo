d5913fe3d829a907124b773eff676cec2c330931
virgesmith <andrew@friarswood.net>
2025-08-31 15:38:08 +0100

    ...

--- src/test/test_cxx_std_invalid.py
+++ src/test/test_cxx_std_invalid.py
@@ -1,4 +1,5 @@
 import platform
+
 import pytest
 
 from inexmo import CompilationError, compile
@@ -7,6 +8,7 @@ from inexmo import CompilationError, compile
 @pytest.mark.skipif(platform.system() == "Windows", reason="cl.exe only warns")
 def test_cxx_std_invalid() -> None:
     with pytest.raises(CompilationError):
+
         @compile(cxx_std=24)
         def f(i: int) -> bool:  # type: ignore[empty-body]
             "return i % 2;"
