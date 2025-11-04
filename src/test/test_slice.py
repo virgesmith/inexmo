d7e0ea0bfa40ff88fc1c68494bf17077b68a4912
virgesmith <andrew@friarswood.net>
2025-11-04 19:17:52 +0000

    adds ellipsis and doc improvements

--- src/test/test_slice.py
+++ src/test/test_slice.py
@@ -1,3 +1,9 @@
+from types import EllipsisType
+
+import numpy as np
+import numpy.typing as npt
+import pytest
+
 from inexmo import compile
 
 
@@ -17,6 +23,28 @@ def parse_slice(length: int, s: slice) -> list[int]:  # type: ignore[empty-body]
     """
 
 
+@compile()
+def slice_shape(a: npt.NDArray[np.float64], *indices: int | slice | EllipsisType) -> list[int]:  # type: ignore[empty-body]
+    """
+    py::array slice = a[indices];
+    return std::vector<int>(slice.shape(), slice.shape() + slice.ndim());
+    """
+
+
+@compile()
+def explicit_ellipsis(a: int | slice | EllipsisType) -> str:  # type: ignore[empty-body]
+    """
+    if (std::get_if<int>(&a)) {
+        return "int";
+    } else if (std::get_if<py::slice>(&a)) {
+        return "slice";
+    } else if (std::get_if<py::ellipsis>(&a)) {
+        return "ellipsis";
+    }
+    throw py::type_error("invalid arg type");
+    """
+
+
 def test_slice() -> None:
     assert parse_slice(10, slice(1, None, 2)) == [1, 3, 5, 7, 9]
     assert parse_slice(10, slice(None, None, -2)) == [9, 7, 5, 3, 1]
@@ -24,5 +52,19 @@ def test_slice() -> None:
     assert parse_slice(10, slice(None, 2, -2)) == [9, 7, 5, 3]
 
 
+def test_ellipsis() -> None:
+    assert slice_shape(np.ones((2, 3, 5, 7)), 1, ..., 2) == [3, 5]
+    assert slice_shape(np.ones((2, 3, 5, 7)), 1, ..., slice(2, 3)) == [3, 5, 1]
+    assert slice_shape(np.ones((2, 3, 5, 7)), ..., 1, slice(2, 4)) == [2, 3, 2]
+    assert slice_shape(np.ones((2, 3, 5, 7)), 1, 2, ...) == [5, 7]
+    assert slice_shape(np.ones((2, 3, 5, 7)), 1, 2, 3, slice(4, None, 2)) == [2]
+
+    assert explicit_ellipsis(1) == "int"
+    assert explicit_ellipsis(slice(1)) == "slice"
+    assert explicit_ellipsis(...) == "ellipsis"
+    with pytest.raises(TypeError):
+        explicit_ellipsis("abc")
+
+
 if __name__ == "__main__":
-    test_slice()
+    test_ellipsis()
